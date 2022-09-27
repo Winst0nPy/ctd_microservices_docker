935daf80936c1229088874965fb9252abd815f41
Daniel Cagliari <dacagliari@gmail.com>
2022-09-26 21:37:36 -0300

    adding new gitignore

--- movie-service/target/classes/movie.sql
+++ movie-service/target/classes/movie.sql
@@ -1,8 +1,15 @@
-insert into movie(name, genre, url_stream) values('IT', 'terror', 'what')
-insert into movie(name, genre, url_stream) values('La masacre de texas', 'terror', 'what')
-insert into movie(name, genre, url_stream) values('Jack el destripador', 'terror', 'what')
-insert into movie(name, genre, url_stream) values('Anabelle', 'terror', 'what')
-insert into movie(name, genre, url_stream) values('Fast and Furious', 'acción', 'what')
-insert into movie(name, genre, url_stream) values('shadow', 'acción', 'what')
-insert into movie(name, genre, url_stream) values('Orgullo y prejuicio', 'romance', 'what')
-insert into movie(name, genre, url_stream) values('The notebook', 'romance', 'what')
+create table movie
+(
+    name int null,
+    genre int null,
+    url_stream int null
+);
+
+insert into movie(name, genre, url_stream) values('IT', 'terror', 'what');
+insert into movie(name, genre, url_stream) values('La masacre de texas', 'terror', 'what');
+insert into movie(name, genre, url_stream) values('Jack el destripador', 'terror', 'what');
+insert into movie(name, genre, url_stream) values('Anabelle', 'terror', 'what');
+insert into movie(name, genre, url_stream) values('Fast and Furious', 'acción', 'what');
+insert into movie(name, genre, url_stream) values('shadow', 'acción', 'what');
+insert into movie(name, genre, url_stream) values('Orgullo y prejuicio', 'romance', 'what');
+insert into movie(name, genre, url_stream) values('The notebook', 'romance', 'what');
